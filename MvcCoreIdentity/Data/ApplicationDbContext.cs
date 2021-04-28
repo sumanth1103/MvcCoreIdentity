e0d5ef09638b5f63caafae898c64bb228b5c8352
Reddipalli <899167@cognizant.com>
2021-04-28 20:16:40 +0530

    Add project files.

---
+++ MvcCoreIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcCoreIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        /*protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=PCIN490774;Initial Catalog=test;Integrated Security=True");
+            }
+        }*/
+    }
+}
